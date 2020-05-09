a0678dfc5706a898057dc070a4812a2295cea5fa
Waldemar Adams <waldek-adamski@wp.pl>
2020-05-09 21:48:30 +0200

    change 'startGame' function

--- main.js
+++ main.js
@@ -3,23 +3,22 @@ const showColors = document.querySelector('div.showColors');
 divColors = [...document.querySelectorAll('.colors div')];
 divFirst = document.querySelector('div.first');
 
-let drawColors = []
-let index = 0;
-
 startGame = function () {
+    let drawColors = [];
+    let index = 0;
+
     start = new Colors();
     drawColors = start.getColors();
-    console.log(drawColors)
+    console.log(drawColors);
 
+    divColors.forEach(function (div) {
+        div.addEventListener('click', function () {
+            if (drawColors[index] === this.dataset.color) {
+                console.log('dobrze!');
+                index++;
+            } else return drawColors = [], index = 0, alert('przegrales')
+        })
+    })
 }
 
-divColors.forEach(function (div) {
-    div.addEventListener('click', function () {
-
-        if (drawColors[index] === this.dataset.color) {
-            console.log('dobrze!');
-            index++;
-        } else alert('Przegrales')
-    })
-})
 btn.addEventListener('click', startGame)
